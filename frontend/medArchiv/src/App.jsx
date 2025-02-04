c92b2e68d4a9825ab3a548123a0c5132dcfd52b5
Tarun790 <tk7091254@gmail.com>
2025-02-04 13:49:36 +0530

    Sidebar

--- frontend/medArchiv/src/App.jsx
+++ frontend/medArchiv/src/App.jsx
@@ -1,17 +1,23 @@
 import { useState } from 'react'
 import './App.css'
-import { Route, Routes } from 'react-router-dom'
+// import { Route, Routes } from 'react-router-dom'
 import DoctorDashboard from './Components/Doctor/DoctorDashboard'
+
 // import {DoctorNotification} from './Components/Doctor/DoctorNotification'
-import DoctorAppointmentDetail from './Components/Doctor/DoctorAppointmentDetail'
+//import DoctorAppointmentDetail from './Components/Doctor/DoctorAppointmentDetail'
 function App() {
   const [count, setCount] = useState(0)
 
+  let changeCount=()=>{
+    setCount(count+1);
+  }
   return (
     <>
-     <Routes>
+     {/* <Routes>
       <Route path="/" element={<DoctorDashboard/>}/>
-     </Routes>
+     </Routes> */}
+     <button onClick={changeCount}>Count {count}</button>
+     <DoctorDashboard/>
     
 
     </>
--- frontend/medArchiv/src/Components/Doctor/DoctorDashboard.jsx
+++ frontend/medArchiv/src/Components/Doctor/DoctorDashboard.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from "react"
+import {Layout } from 'antd'; 
+import Logo from './Logo'
+import MenuList from "./MenuList";
+import ToggleThemeButton from "./ToggleThemeButton";
 
+const{Header, Sider } = Layout;
 function DoctorDashboard() {
+  const [darkTheme, setDarkTheme] = useState(true)
+  const toggleTheme= () => {
+    setDarkTheme(!darkTheme);
+  }
   return (
-    <div>
-      <h1>Tarun</h1>
-    </div>
+    <Layout>
+      <Sider theme ={darkTheme ? 'dark' : 'light'}  className= "sidebar">
+        <Logo/>
+        <MenuList darkTheme = {darkTheme}/>
+        <ToggleThemeButton darkTheme={darkTheme}
+        toggleTheme={toggleTheme}/>
+        </Sider>
+        <Layout></Layout>
+      </Layout>
+    
   )
 }
 
